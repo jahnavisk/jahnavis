658fe2ad64a26386b7daad455fe8a31db614485b
jahnavisk <116362007+jahnavisk@users.noreply.github.com>
2022-10-21 18:37:25 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,15 @@
+package program1;
+
+import java.util.*;
+
+public class Oddnumbers {
+	
+	public static void main(String [] args){
+	 int a=4;
+	for (int i=1;i<=7; i=i+2)
+	
+	{
+		System.out.println(i);  
+	}
+	}}
+	
